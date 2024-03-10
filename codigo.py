9fc57aaab813c494ecd8113931e9f00a47f0a3b3
ingrid <kamilaingridc@gmail.com>
2024-03-10 08:23:35 -0300

    finished

--- codigo.py
+++ codigo.py
@@ -3,9 +3,41 @@ import flet as ft  # importar
 def main(pagina):  # cria a função principal 
     texto = ft.Text("ZapZap")
 
+    chat = ft.Column()
+
+    def enviar_mensagem_tunel(mensagem):
+        # adc msg no chat 
+        texto_mensagem = ft.Text(mensagem)
+        chat.controls.append(texto_mensagem)
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+
+    def enviar_mensagem(evento):
+        pagina.pubsub.send_all(f"{nome_usuario.value}: {campo_mensagem.value}")
+        # limpa campo de mensagem 
+        campo_mensagem.value=""
+        pagina.update()
+
+    campo_mensagem = ft.TextField(label="Type ur message", on_submit=enviar_mensagem)
+    botao_enviar = ft.ElevatedButton("Send", on_click=enviar_mensagem)
+    linha_enviar = ft.Row([campo_mensagem, botao_enviar])
+
     def entrar_chat(evento):
         print("U're in chatZap.")
+        # fechar o popup
         popup.open = False
+        # tirar o botao 
+        pagina.remove(botao_iniciar)
+        # tirar o titulo 
+        pagina.remove(texto)
+        # criar o chat
+        pagina.add(chat)
+        pagina.pubsub.send_all(f"{nome_usuario.value} entrou no chat")
+        # campo de digitar
+        pagina.add(linha_enviar) 
+
+        pagina.update()
 
     titulo_popup = ft.Text("Welcome to ZapZap!")
     nome_usuario = ft.TextField(label="Write ur name")
@@ -28,4 +60,4 @@ def main(pagina):  # cria a função principal
     pagina.add(texto)
     pagina.add(botao_iniciar)
 
-ft.app(target=main)  # criar o app chamando a função principal
+ft.app(target=main, view=ft.WEB_BROWSER)  # criar o app chamando a função principal
